de2ddadbf0ad2ab0f13dbbfa2dc0216220eccda9
Bideo Wego <bideowego@gmail.com>
2018-08-09 03:04:19 -0400

    Add snare

--- public/assets/javascripts/bpm.js
+++ public/assets/javascripts/bpm.js
@@ -6,7 +6,14 @@ var bpm = function (options) {
 
   bpm._ = this;
   Object.assign(this, options || {}, bpm._defaults);
-  bpm.reset();
+
+  this._intervalId = null;
+  this._lastTimestamp = null;
+  this._tickAmount = ((1000 * 60) / this.tempo) / this.baseDuration;
+  this._elaspedTime = 0;
+  this._eventListeners = {};
+  this._currentNumerator = 0;
+  this._currentMeasure = 0;
 }
 
 bpm._defaults = {
@@ -21,9 +28,7 @@ bpm.reset = function() {
   var instance = bpm._;
   instance._intervalId = null;
   instance._lastTimestamp = null;
-  instance._tickAmount = ((1000 * 60) / instance.tempo) / instance.baseDuration;
   instance._elaspedTime = 0;
-  instance._eventListeners = {};
   instance._currentNumerator = 0;
   instance._currentMeasure = 0;
 };
@@ -50,6 +55,14 @@ bpm.on = function(eventType, callback) {
   listeners.push(callback);
 };
 
+bpm.position = function() {
+  // TODO make this a setter or getter
+  var instance = bpm._;
+  var n = instance._currentNumerator;
+  var m = instance._currentMeasure;
+  return { n: n, m: m };
+};
+
 bpm._update = function() {
   var instance = bpm._;
   var currentTimestamp = Date.now();
@@ -72,8 +85,8 @@ bpm._fire = function() {
     instance._currentNumerator = 0;
     instance._currentMeasure += 1;
   }
-  instance._currentNumerator += 1;
   instance._eventListeners.quarter.forEach(function(callback) {
-    callback();
+    callback(instance._currentNumerator, instance._currentMeasure);
   });
+  instance._currentNumerator += 1;
 };
